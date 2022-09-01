ed019d2989fb9da24e10f8020ae3b803f6eb634a
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-01 21:27:10 +0300

    Introduction to laravel view

--- app/Http/Controllers/Test/NewsController.php
+++ app/Http/Controllers/Test/NewsController.php
@@ -25,7 +25,7 @@ class NewsController extends Controller
      */
     public function create()
     {
-        //
+        return view(view: 'test\news\create');
     }
 
     /**
---
+++ resources/views/test/news/create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create News</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.js') }}">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <h1>Create News</h1>
+            <div class="col-12">
+                <label for="name">Name:</label>
+                <input type="text" id="name" placeholder="Enter The Name">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
