cca1f740471784890036b52ff060cbd3e0137740
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-01 14:58:48 +0300

    Routing: New files for Admin Routes

---
+++ routes/admin.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "admin" middleware group. Now create something great!
+|
+*/
+
+Route::get('/admin', function () {
+    return "Hello from Admin Role.";
+});
